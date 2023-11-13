4c9a88932ba6d2853e8d2a45ea18b10fcabad0a6
Katalin Kovács <kovacs.katalin89@gmail.com>
2023-11-13 16:39:41 +0100

    add board component

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SquareComponent } from '../square/square.component';
+
+@Component({
+  selector: 'app-board',
+  standalone: true,
+  imports: [CommonModule, SquareComponent],
+  templateUrl: './board.component.html',
+  styleUrl: './board.component.scss',
+})
+export class BoardComponent implements OnInit {
+  squares!: any[];
+  xIsNext!: boolean;
+  winner!: string;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.newGame();
+  }
+
+  newGame() {
+    this.squares = Array(9).fill(null);
+    this.winner = '';
+    this.xIsNext = true;
+  }
+
+  get player() {
+    return this.xIsNext ? 'X' : 'O';
+  }
+
+  //event handler for clicking the board buttons
+  //to make a move
+  makeMove(idx: number) {
+    //if the square is already clicked do nothing
+    //if the square is empty or null
+    //than splice with the index of the square clicked on
+    //and with the current player
+    //and toggle the xIsNext to its opposite value
+    if (!this.squares[idx]) {
+      this.squares.splice(idx, 1, this.player);
+      this.xIsNext = !this.xIsNext;
+    }
+
+    this.winner = this.calculateWinner();
+  }
+
+  calculateWinner() {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        this.squares[a] &&
+        this.squares[a] === this.squares[b] &&
+        this.squares[a] === this.squares[c]
+      ) {
+        return this.squares[a];
+      }
+    }
+    return null;
+  }
+}
